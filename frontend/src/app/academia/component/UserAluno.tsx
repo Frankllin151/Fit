4f854859b69117f7050eab9f1bd572831be70c88
Frankllin151 <frankllinsilva300@gmail.com>
2024-01-11 16:01:13 -0300

    style:planoAcademiamodificado

---
+++ frontend/src/app/academia/component/UserAluno.tsx
@@ -0,0 +1,10 @@
+export const UserPeople = () =>{
+   return (
+    <div className="px-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+
+            
+        </div>
+    </div>
+   )
+}
--- frontend/src/app/academia/mensalidades/page.tsx
+++ frontend/src/app/academia/mensalidades/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { UserPeople } from "../component/UserAluno";
 import Header from "../header";
 import {useState} from "react";
 
@@ -19,35 +20,31 @@ const  Page = () =>{
     <div  className={isSidebarActive ? 'ml-64' : ''}>
       
        
-    <div className="flex justify-center">
-    <div className="grid xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-8">
+    <div className="px-10">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {/* Plano 3 meses */}
-        <div className="bg-indigo-500 py-5 px-3 
-            rounded-md hover:opacity-70 
-            cursor-pointer">
-            <p>R$150 à vista</p>
-            <p>R$53,40 no cartão</p>
+        <div className=" bg-indigo-500 py-5 px-3 rounded-md hover:opacity-70 cursor-pointer">
+            <p className="font-sans font-semibold">R$150 à vista</p>
+            <p className="text-sm">R$53,40 no cartão</p>
         </div>
 
         {/* Plano 6 meses */}
-        <div className="bg-indigo-500 py-5 px-3 
-            rounded-md hover:opacity-70 
-            cursor-pointer">
-            <p>R$300 à vista</p>
-            <p>R$106,80 no cartão</p>
+        <div className="bg-indigo-500 py-5 px-3 rounded-md hover:opacity-70 cursor-pointer">
+            <p className=" font-sans font-semibold">R$300 à vista</p>
+            <p className="text-sm">R$106,80 no cartão</p>
         </div>
 
         {/* Plano anual */}
-        <div className="bg-indigo-500 py-5 px-3 
-            rounded-md hover:opacity-70 
-            cursor-pointer">
-            <p>R$600 à vista</p>
-            <p>R$213,60 no cartão</p>
+        <div className="bg-indigo-500 py-5 px-3 rounded-md hover:opacity-70 cursor-pointer">
+            <p className="font-sans font-semibold">R$600 à vista</p>
+            <p className="text-sm">R$213,60 no cartão</p>
         </div>
     </div>
 </div>
 
+        <UserPeople/>
     </div>
+
     </>
     )
   }
